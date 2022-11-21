6d32779c2c85bbec07ca840fd3f4d47c62015ef5
Plasticine001 <cxx0605@outlook.com>
2022-11-21 21:42:47 +0000

    test

--- js/test.js
+++ js/test.js
@@ -175,15 +175,15 @@ function getPosition(event){
     // dis_x = arr_vel_x[x][Math.abs(256-y)]/2.0+127;
     // dis_y = arr_vel_y[x][Math.abs(256-y)]/2.0+127;
 
-    // dis_x = arr_vel_x[x][y];
-    // dis_y = arr_vel_y[x][y];
-    // new_x = x-dis_y;
-    // new_y = y-dis_x;
+    dis_x = arr_vel_x[x][y]/127.5*128;
+    dis_y = arr_vel_y[x][y]/127.5*128;
+    new_x = x-dis_y;
+    new_y = y-dis_x;
 
-    dis_x = arr_vel_x[255-y][x];
-    dis_y = arr_vel_y[255-y][x];
-    new_x = dis_x;
-    new_y = 256-dis_y;
+    // dis_x = arr_vel_x[255-y][x];
+    // dis_y = arr_vel_y[255-y][x];
+    // new_x = dis_x;
+    // new_y = 256-dis_y;
 
     // new_x = dis_x;
     // new_y = Math.abs(255-dis_y);
